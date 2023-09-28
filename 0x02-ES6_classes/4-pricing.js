22c4d20b489aa60388c48e1dda241c64caefee6e
masha-a-m <escritoramaree@gmail.com>
2023-09-28 09:10:48 -0700

    u

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,23 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
